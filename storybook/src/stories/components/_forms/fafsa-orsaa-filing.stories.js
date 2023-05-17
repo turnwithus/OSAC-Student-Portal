e94a7c1bd80130d26bd3094cd673d265fd16bd09
Jeremiah Bascue <jeremiah@turnwith.us>
2023-05-16 19:46:46 -0700

    Updates Storybook nav for FAFSA/ORSAA form composition for consistency

--- storybook/src/stories/components/_forms/fafsa-orsaa-filing.stories.js
+++ storybook/src/stories/components/_forms/fafsa-orsaa-filing.stories.js
@@ -6,7 +6,7 @@ import { InputRadio } from '../../blocks/input-radio/input-radio';
 import { ProgressBar } from '../progress-bar/progress-bar';
 
 export default {
-  title: 'Forms/Fafsa Orsaa',
+  title: 'Forms/Fafsa Orsaa Filing',
 };
 
 export const FafsaOrsaaFiling = () => {
